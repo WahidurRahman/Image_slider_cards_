51166b4491b9e82ebf37d7cf1c96ef17a9f78a56
WahidurRahman <wrahmanj@gmail.com>
2021-07-02 21:14:56 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach(panel => {
+  panel.addEventListener("click", e => {
+    removeActivePanel();
+    panel.classList.add("active");
+  });
+});
+
+function removeActivePanel() {
+  panels.forEach(panel => {
+    panel.classList.remove("active");
+  });
+}
